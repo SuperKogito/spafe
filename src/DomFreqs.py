e6cf692697f1c0fa31f39c3d97cb954bed67409d
SuperKogito <kogito@hotmail.fr>
2019-09-23 18:30:51 +0200

    update and comment code

--- src/DomFreqs.py
+++ src/DomFreqs.py
@@ -8,7 +8,20 @@ class DominantFrequenciesExtractor:
         self.debug = debug
 
     def sliding_window(self, signal, rate, window_length_in_ms, skip_in_ms):
-        window_length = int(rate / 1000) * window_length_in_ms  
+        """
+        Return slices of a signal based on a sliding winding concept with a 
+        defined window width and window step.
+        
+        Args: 	    
+            sig       (array) : audio signal (list of float)
+            rate        (int) : sampling rate (= average number of samples pro 1 second)
+            window_length_in_ms (int) : slicing window in milli-seconds.
+            skip_in_ms          (int) : slicing step in milli-seconds.
+
+        Returns: 	    
+            (array) : array window/slice.
+        """
+        window_length = int(rate / 1000) * window_length_in_ms 
         window_skip   = int(rate / 1000) * skip_in_ms
         
         if window_skip is None: window_skip = window_length
@@ -16,6 +29,18 @@ class DominantFrequenciesExtractor:
                 yield signal[i: i + window_length]
                 
     def split_signal_in_frames(self, signal, rate, frame_size_in_ms = 100):
+        """
+        Return slices of a signal based on a sliding winding concept with a 
+        fixed window width and window step.
+        
+        Args: 	    
+            sig       (array) : audio signal (list of float)
+            rate        (int) : sampling rate (= average number of samples pro 1 second)
+            frame_size_in_ms (int) : slicing window and step in milli-seconds.
+
+        Returns: 	    
+            (list) : list of signal slices.
+        """
         samples_pro_frame       = int(rate / 1000) * frame_size_in_ms
         samples_count_pro_frame = int(int(len(signal) / samples_pro_frame) * samples_pro_frame)
         frames                  = np.split(signal[:samples_count_pro_frame],
@@ -26,7 +51,14 @@ class DominantFrequenciesExtractor:
     
     def get_dominant_frequencies(self, slices, rate):
         """
-        Returns the dominant audio frequency of a given frame in hertz.
+        Returns the dominant audio frequency (in Hertz) of a given audio signal.
+            
+        Args: 	    
+            slices  (list) : list of signal slices.
+            rate     (int) : sampling rate (= average number of samples pro 1 second)
+
+        Returns: 	    
+            (array) : array of dominant frequencies.
         """        
         dominant_frequencies = []
         for sig in [sig for sig in slices if sig.size > 0]:
@@ -47,12 +79,19 @@ class DominantFrequenciesExtractor:
                 plt.ylim(psd.min(), psd.max() + .1 * psd.max())
                 plt.xlim(-.25, dominant_frequencies[-1] /1000 + .25*dominant_frequencies[-1] /1000)
                 plt.show()
-                
         return np.array(dominant_frequencies)
         
     def main(self, file_name):
+        """
+        Returns the dominant audio frequencies of a given wave file.
+            
+        Args: 	    
+            file_name (str) : name of an audio file name.
+
+        Returns: 	    
+            (array) : array of dominant frequencies.
+        """     
         signal, rate = librosa.load(file_name)
-        signal       = signal[:rate*20:100]
         
         # apply Band pass Butterworth filter
         lower_cutoff, upper_cutoff = 50, 3000
@@ -61,11 +100,11 @@ class DominantFrequenciesExtractor:
         signal = scipy.signal.lfilter(b, a, signal) 
 
         # compute dominant frequencies               
-        slices    = self.split_signal_in_frames(signal, rate)
         slices    = [w for w in self.sliding_window(signal, rate, 10, 5)] 
         dom_freqs = self.get_dominant_frequencies(slices, rate)
         return dom_freqs
     
 # test 
-dominant_frequencies_extractor = DominantFrequenciesExtractor(debug=True)
-dominant_frequencies           = dominant_frequencies_extractor.main("voc0U0t10dfnzl.wav")
+if __name__ == '__main__':
+    dominant_frequencies_extractor = DominantFrequenciesExtractor(debug=False)
+    dominant_frequencies           = dominant_frequencies_extractor.main("sample2.wav")
--- src/FundFreqs.py
+++ src/FundFreqs.py
@@ -1,3 +1,9 @@
+"""
+Credits to: 
+    Patrice Guyot. (2018, April 19). 
+    Fast Python implementation of the Yin algorithm (Version v1.1.1). 
+    Zenodo. http://doi.org/10.5281/zenodo.1220947
+"""
 import time
 import numpy as np
 from scipy.io import wavfile
@@ -6,8 +12,8 @@ from scipy.signal import fftconvolve
 
 
 class FundamentalFrequencies:
-    def __init__(self):
-        pass
+    def __init__(self, debug=False):
+        self.debug = debug
 
     def differenceFunction(self, x, N, tau_max):
         """
@@ -23,22 +29,11 @@ class FundamentalFrequencies:
         Returns: 	    
             (list) : difference function
         """
-#        x = np.array(x, np.float64)
-#        w = x.size
-#        tau_max  = min(tau_max, w)
-#        x_cumsum = np.concatenate((np.array([0.]), (x * x).cumsum()))
-#        size = w + tau_max
-#        p2   = (size // 32).bit_length()
-#        nice_numbers = (16, 18, 20, 24, 25, 27, 30, 32)
-#        size_pad     = min(x * 2 ** p2 for x in nice_numbers if x * 2 ** p2 >= size)
-#        fc   = np.fft.rfft(x, size_pad)
-#        conv = np.fft.irfft(fc * fc.conjugate())[:tau_max]
-#        return x_cumsum[w:w - tau_max:-1] + x_cumsum[w] - x_cumsum[:tau_max] - 2 * conv
         x = np.array(x, np.float64)
         w = x.size
         x_cumsum = np.concatenate((np.array([0]), (x * x).cumsum()))
-        conv = fftconvolve(x, x[::-1])
-        tmp = x_cumsum[w:0:-1] + x_cumsum[w] - x_cumsum[:w] - 2 * conv[w - 1:]
+        conv     = fftconvolve(x, x[::-1])
+        tmp      = x_cumsum[w:0:-1] + x_cumsum[w] - x_cumsum[:w] - 2 * conv[w - 1:]
         return tmp[:tau_max]
 
 
@@ -55,7 +50,6 @@ class FundamentalFrequencies:
         Returns: 	    
             (list) : cumulative mean normalized difference function
         """
-    
         cmndf = df[1:] * range(1, N) / np.cumsum(df[1:]).astype(float) # scipy method
         return np.insert(cmndf, 0, 1)
     
@@ -87,25 +81,24 @@ class FundamentalFrequencies:
         """
         Compute the Yin Algorithm. Return fundamental frequency and harmonic rate.
         
-        Parameters
-        ----------
-        sig: Audio signal (list of float)
-        sr: sampling rate (int)
-        w_len: size of the analysis window (samples)
-        w_step: size of the lag between two consecutives windows (samples)
-        f0_min: Minimum fundamental frequency that can be detected (hertz)
-        f0_max: Maximum fundamental frequency that can be detected (hertz)
-        harmo_tresh: Threshold of detection. The yalgorithmù return the first minimum of the CMND fubction below this treshold.
-        :returns:
-            * pitches: list of fundamental frequencies,
-            * harmonic_rates: list of harmonic rate values for each fundamental frequency value (= confidence value)
-            * argmins: minimums of the Cumulative Mean Normalized DifferenceFunction
-            * times: list of time of each estimation
-        :rtype: tuple
+        Args:
+            sig        (list) : Audio signal (list of float)
+            sr          (int) : sampling rate (= average number of samples pro 1 second)
+            w_len       (int) : size of the analysis window (in #samples)
+            w_step      (int) : size of the lag between two consecutives windows (in #samples)
+            f0_min      (int) : Minimum fundamental frequency that can be detected (in Hertz)
+            f0_max      (int) : Maximum fundamental frequency that can be detected (in Hertz)
+            harmo_tresh (int) : Threshold of detection. The yalgorithmù return the first minimum of the CMND fubction below this threshold.
+        
+        Returns:
+            (tuple) : tuple include the following
+                          pitches       : list of fundamental frequencies,
+                          harmonic_rates: list of harmonic rate values for each fundamental frequency value (= confidence value)
+                          argmins       : minimums of the Cumulative Mean Normalized DifferenceFunction
+                          times         : list of time of each estimation
         """
-        print('Yin: compute yin algorithm')
-        tau_min = int(sr / f0_max)
-        tau_max = int(sr / f0_min)
+        if self.debug: print('Yin: compute yin algorithm')
+        tau_min, tau_max = int(sr / f0_max), int(sr / f0_min)
     
         timeScale = range(0, len(sig) - w_len, w_step)  #  time values for each analysis window
         times     = [t / float(sr)    for t in timeScale]
@@ -134,53 +127,55 @@ class FundamentalFrequencies:
                 
         return pitches, harmonic_rates, argmins, times
     
-    def main(self, sr, sig, w_len=1024, w_step=256, f0_min=70, f0_max=200, harmo_thresh=0.85, audioDir="./", dataFileName=None, verbose=4):
+    def main(self, sr, sig, w_len=1024, w_step=256, f0_min=70, f0_max=200, harmo_thresh=0.85, audioDir="./", dataFileName=None):
         """
         Run the computation of the Yin algorithm on a example file.
-        Write the results (pitches, harmonic rates, parameters ) in a numpy file.
-        audioFileName: name of the audio file
-        :type audioFileName: str
-        w_len: length of the window
-        :type wLen: int
-        wStep: length of the "hop" size
-        :type wStep: int
-        f0_min: minimum f0 in Hertz
-        :type f0_min: float
-        f0_max: maximum f0 in Hertz
-        :type f0_max: float
-        harmo_thresh: harmonic threshold
-        :type harmo_thresh: float
-        audioDir: path of the directory containing the audio file
-        :type audioDir: str
-        dataFileName: file name to output results
-        :type dataFileName: str
-        verbose: Outputs on the console : 0-> nothing, 1-> warning, 2 -> info, 3-> debug(all info), 4 -> plot + all info
-        :type verbose: int
-        """
-        start = time.time()
-        pitches, harmonic_rates, argmins, times = self.compute_yin(sig, sr, dataFileName, w_len, w_step, f0_min, f0_max, harmo_thresh)
-        print ("Yin computed in: ", time.time() - start)
-    
-        duration = len(sig)/float(sr)
-        plt.figure(figsize=(20, 10))
-        plt.subplots_adjust(left   = 0.125,  right  = 0.9,   
-                            bottom = 0.1,    top    = 0.9,     
-                            wspace = 0.2,    hspace = 0.99)
         
-        if verbose >3:
+        Args:
+            sig        (list) : Audio signal (list of float)
+            sr          (int) : sampling rate (= average number of samples pro 1 second)
+            w_len       (int) : size of the analysis window (in #samples)
+            w_step      (int) : size of the lag between two consecutives windows (in #samples)
+            f0_min      (int) : Minimum fundamental frequency that can be detected (in Hertz)
+            f0_max      (int) : Maximum fundamental frequency that can be detected (in Hertz)
+            harmo_tresh (int) : Threshold of detection. The yalgorithmù return the first minimum of the CMND fubction below this threshold.
+        
+        Returns:
+            (tuple) : tuple include the following
+                          pitches       : list of fundamental frequencies,
+                          harmonic_rates: list of harmonic rate values for each fundamental frequency value (= confidence value)
+                          argmins       : minimums of the Cumulative Mean Normalized DifferenceFunction
+                          times         : list of time of each estimation        
+        """
+        start    = time.time()
+        duration = len(sig) / float(sr)
+        pitches, harmonic_rates, argmins, times = self.compute_yin(sig, sr, dataFileName, w_len, w_step, f0_min, f0_max, harmo_thresh)
+        if self.debug:
+            print ("Yin computed in: ", time.time() - start)
+            plt.figure(figsize=(20, 10))
+            plt.subplots_adjust(left   = 0.125,  right  = 0.9,   
+                                bottom = 0.1,    top    = 0.9,     
+                                wspace = 0.2,    hspace = 0.99)
+            # plot audio data 
             ax1 = plt.subplot(4, 1, 1)
             ax1.plot([float(x) * duration / len(sig) for x in range(0, len(sig))], sig)
             ax1.set_title('Audio data')
             ax1.set_ylabel('Amplitude')
+            
+            # plot F0
             ax2 = plt.subplot(4, 1, 2)
             ax2.plot([float(x) * duration / len(pitches) for x in range(0, len(pitches))], pitches)
             ax2.set_title('F0')
             ax2.set_ylabel('Frequency (Hz)')
+            
+            # plot Harmonic rate
             ax3 = plt.subplot(4, 1, 3, sharex=ax2)
             ax3.plot([float(x) * duration / len(harmonic_rates) for x in range(0, len(harmonic_rates))], harmonic_rates, "-x")
             ax3.plot([float(x) * duration / len(harmonic_rates) for x in range(0, len(harmonic_rates))], [harmo_thresh] * len(harmonic_rates), 'r', "--")
             ax3.set_title('Harmonic rate')
             ax3.set_ylabel('Rate')
+            
+            # plot Index of minimums of CMND
             ax4 = plt.subplot(4, 1, 4, sharex=ax2)
             ax4.plot([float(x) * duration / len(argmins) for x in range(0, len(argmins))], argmins, "-x")
             ax4.set_title('Index of minimums of CMND')
@@ -188,48 +183,10 @@ class FundamentalFrequencies:
             ax4.set_xlabel('Time (seconds)')
             plt.show()
     
-        return np.array(pitches), harmonic_rates, argmins, times, duration
+        return np.array(pitches), harmonic_rates, argmins, times
 
 if __name__ == '__main__':
     #  read signal, convert from bits and select from signal 
-    freq, sig               = wavfile.read('sample2.wav')
-    sig = sig[:100000]
-    fundamental_frequencies = FundamentalFrequencies()
-    pitches, harmonic_rates, argmins, times, duration = fundamental_frequencies.main(freq, sig)
-
-    #  prepare time array
-    time_array  = np.arange(0, sig.shape[0], 1) / freq
-    time_array *= 1000                                   # scale to milliseconds
-    
-    #  plot the tone
-    plt.plot(time_array[::10], sig[::10])
-    plt.ylabel('Amplitude')
-    plt.xlabel('Time (ms)')
-    plt.show()
-    
-    #  comput fft and psd
-    N                  = len(sig) 
-    fourrier_transform = np.fft.fft(sig) #  take the fourier transform 
-    psd                = (1 / N)* abs(fourrier_transform)**2  #  square it to get the power 
-    freq_array         = np.arange(0, N, 1.0) * (freq / N)
-   
-    #  plot power density
-    plt.plot(freq_array[::100], 10*np.log10(fourrier_transform)[::100], "-")
-    plt.xlabel('Frequency (Hz)')
-    plt.ylabel('Power (dB)')
-    plt.show()
-
-    #  plot spectogram    
-    plt.specgram(sig, Fs=freq)
-    plt.plot([float(x) * duration / len(pitches) for x in range(0, len(pitches))], pitches)
-    plt.xlabel('Time')
-    plt.ylabel('Frequency (Hz)')
-    plt.ylim(0,5000)
-    plt.show()
-    
-    plt.figure(figsize=(20, 10))
-    duration = len(sig)/float(freq)
-    plt.stem([float(x) * duration / len(pitches) for x in range(0, len(pitches))][::], pitches[::],  markerfmt=' ',use_line_collection=True)
-    plt.title('F0')
-    plt.ylabel('Frequency (Hz)')
-    plt.show()
+    rate, sig               = wavfile.read('sample2.wav')
+    fundamental_frequencies = FundamentalFrequencies(False)
+    pitches, harmonic_rates, argmins, times = fundamental_frequencies.main(rate, sig)
