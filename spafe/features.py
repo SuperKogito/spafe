632ec0e8fa72506a2e59c8412111750148278b5c
SuperKogito <kogito@hotmail.fr>
2019-09-26 11:34:10 +0200

    add mfe computations

--- spafe/features.py
+++ spafe/features.py
@@ -6,12 +6,15 @@ import numpy as np
 class FeaturesExtractor:
     def __init__(self, signal, rate):
         self.signal, self.rate = signal, rate
+        
     def get_mfcc_features(self, signal, rate):
-        # Extract MFCCs, MFCC deltas, MFCC dosuble deltas
+        melspectrogram = librosa.feature.melspectrogram(y=self.signal, sr=self.rate, n_mels=80, fmax=8000) 
+        # Extract MFCCs, MFCC deltas, MFCC dosuble deltas and MFECs
         mfcc        = librosa.feature.mfcc(signal, n_mfcc=13)
         delta_mfcc  = librosa.feature.delta(mfcc)
         delta2_mfcc = librosa.feature.delta(mfcc, order=2)
-        return [mfcc, delta_mfcc, delta2_mfcc]
+        mfec        = librosa.power_to_db(melspectrogram) 
+        return [mfec, mfcc, delta_mfcc, delta2_mfcc]
 
     def get_linear_features(self, signal, rate):
         # Extract LPCs, LPCCs and LSPs
