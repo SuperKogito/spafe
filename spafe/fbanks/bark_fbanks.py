75819c06eb0afdc0b1a258042fb4eb9f1cf56b61
SuperKogito <kogito@hotmail.fr>
2019-10-07 18:56:44 +0200

    add functional gammatone fbanks and update other fbanks

--- spafe/fbanks/bark_fbanks.py
+++ spafe/fbanks/bark_fbanks.py
@@ -2,83 +2,26 @@
 #                      Bark-filter-banks implementation
 ################################################################################
 import numpy as np
+from spafe.utils.converters import hz2bark, fft2bark, bark2fft
 
-
-def hz2bark(f):
-    """
-    Convert Hz frequencies to Bark.
-    
-    Args:
-    -----
-    f (np.array) : input frequencies [Hz].
-    
-    Returns:
-    --------
-    fb (np.array): frequencies in Bark [Bark].
-    """
-    return 6. * np.arcsinh(f / 600. )  
-
-def bark2hz(fb):
-    """
-    Convert Bark frequencies to Hz.
-    
-    Args:
-    -----
-    fb (np.array) : input frequencies [Bark].
-    
-    Returns:
-    --------
-    f (np.array)  : frequencies in Hz [Hz].
-    """
-    return 600. * np.sinh( fb / 6.)
-
-def fft2bark(fft, fs=16000, nfft=512):
-    """
-    Convert Bark frequencies to Hz.
-    
-    Args:
-    -----
-    fft (np.array) : fft bin numbers.
-    
-    Returns:
-    --------
-    fb (np.array): frequencies in Bark [Bark].
-    """
-    return hz2bark((fft * fs) / (nfft + 1))
-
-def bark2fft(fb, fs=16000, nfft=512):   
-    """
-    Convert Bark frequencies to fft bins.
-    
-    Args:
-    -----
-    fb (np.array): frequencies in Bark [Bark].
-    
-    Returns:
-    --------
-    fft (np.array) : fft bin numbers.
-    """
-    return (nfft + 1) * bark2hz(fb) / fs
 
 def Fm(fb, fc):
     """
-    Compute bark filter around a certain frequency in bark
+    Compute bark filter around a certain frequency in bark.
 
     Args:
-    -----
-    fb (int): frequency in Bark [Bark].
-    fc (int): center frequency in Bark [Bark].
+        fb (int): frequency in Bark [Bark].
+        fc (int): center frequency in Bark [Bark].
 
     Returns:
-    --------
-    (float) : associated Bark filter value/amplitude.
+        (float) : associated Bark filter value/amplitude.
     """
     if   fc - 2.5 <= fb <= fc - 0.5 : return 10**(2.5 * (fb - fc + 0.5))
     elif fc - 0.5 <  fb <  fc + 0.5 : return 1
     elif fc + 0.5 <= fb <= fc + 1.3 : return 10**(-2.5 * (fb - fc - 0.5))
     else                            : return 0
     
-def bark_filter_banks(nfilt=20, nfft=512, fs=16000, lowfreq=0, highfreq=None):
+def bark_filter_banks(nfilts=20, nfft=512, fs=16000, lowfreq=0, highfreq=None):
     """
     Compute a Bark-filterbank. The filters are stored in the rows, the columns correspond
     to fft bins. The filters are returned as an array of size nfilt * (nfft/2 + 1)
@@ -98,14 +41,14 @@ def bark_filter_banks(nfilt=20, nfft=512, fs=16000, lowfreq=0, highfreq=None):
     # compute points evenly spaced in bark
     lowbark    = hz2bark(lowfreq)
     highbark   = hz2bark(highfreq)
-    barkpoints = np.linspace(lowbark, highbark, nfilt + 4)
+    barkpoints = np.linspace(lowbark, highbark, nfilts + 4)
 
     # The frequencies array/ points are in Bark, but we use fft bins, so we 
     # have to convert from Bark to fft bin number
     bin   = np.floor(bark2fft(barkpoints))
-    fbank = np.zeros([nfilt, nfft // 2 + 1])
+    fbank = np.zeros([nfilts, nfft // 2 + 1])
 
-    for j in range(2, nfilt + 2):
+    for j in range(2, nfilts + 2):
         for i in range(int(bin[j-2]), int(bin[j+2])):
             fc            = barkpoints[j]
             fb            = fft2bark(i)
--- spafe/fbanks/mel_fbanks.py
+++ spafe/fbanks/mel_fbanks.py
@@ -2,33 +2,10 @@
 #                      Mel-filter-banks implementation
 ################################################################################
 import numpy as np
+from spafe.utils.converters import hz2mel, mel2hz
 
 
-def hz2mel(hz):
-    """
-    Convert a value in Hertz to Mels
-
-    Args:
-         hz: a value in Hz. This can also be a numpy array, conversion proceeds element-wise.
-
-    Returns:
-        a value in Mels. If an array was passed in, an identical sized array is returned.
-    """
-    return 2595 * np.log10(1 + hz / 700.)
-
-def mel2hz(mel):
-    """
-    Convert a value in Mels to Hertz
-
-    Args:
-        mel: a value in Mels. This can also be a numpy array, conversion proceeds element-wise.
-
-    Returns:
-        a value in Hertz. If an array was passed in, an identical sized array is returned.
-    """
-    return 700 * (10**(mel / 2595.0) - 1)
-
-def mel_filter_banks(nfilt=20, nfft= 512, fs=16000, lowfreq=0, highfreq=None):
+def mel_filter_banks(nfilts=20, nfft= 512, fs=16000, lowfreq=0, highfreq=None):
     """
     Compute a Mel-filterbank. The filters are stored in the rows, the columns correspond
     to fft bins. The filters are returned as an array of size nfilt * (nfft/2 + 1)
@@ -48,13 +25,16 @@ def mel_filter_banks(nfilt=20, nfft= 512, fs=16000, lowfreq=0, highfreq=None):
     # compute points evenly spaced in mels
     lowmel    = hz2mel(lowfreq)
     highmel   = hz2mel(highfreq)
-    melpoints = np.linspace(lowmel, highmel, nfilt + 2)
+    melpoints = np.linspace(lowmel, highmel, nfilts + 2)
     
     # our points are in Hz, but we use fft bins, so we have to convert from Hz to fft bin number
     bin   = np.floor((nfft + 1) * mel2hz(melpoints) / fs)
-    fbank = np.zeros([nfilt, nfft // 2 + 1])
+    fbank = np.zeros([nfilts, nfft // 2 + 1])
 
-    for j in range(0, nfilt):
-        for i in range(int(bin[j]),   int(bin[j+1])): fbank[j,i] = (i - bin[j]) / (bin[j+1]-bin[j])
-        for i in range(int(bin[j+1]), int(bin[j+2])): fbank[j,i] = (bin[j+2]-i) / (bin[j+2]-bin[j+1])
+    for j in range(0, nfilts):
+        for i in range(int(bin[j]),   int(bin[j+1])): 
+            fbank[j,i] = (i - bin[j]) / (bin[j+1]-bin[j])
+        
+        for i in range(int(bin[j+1]), int(bin[j+2])): 
+            fbank[j,i] = (bin[j+2]-i) / (bin[j+2]-bin[j+1])
     return fbank
