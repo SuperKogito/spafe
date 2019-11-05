beee8dd6cf96956c3c10defc4a08542a0bf6d81e
SuperKogito <kogito@hotmail.fr>
2019-11-05 17:57:55 +0100

    add tests utils modules

---
+++ tests/test_utils_converters.py
@@ -0,0 +1,94 @@
+import pytest
+import numpy as np
+from spafe.utils.exceptions import ParameterError, ErrorMsgs
+from spafe.utils.converters import (hz2erb, erb2hz, hz2bark, bark2hz, hz2mel,
+                                    mel2hz, fft2hz, fft2erb, erb2fft, hz2fft,
+                                    fft2bark, bark2fft)
+
+
+@pytest.fixture
+def fhz():
+    return np.array([
+        20., 160., 394., 670., 1000., 1420., 1900., 2450., 3120., 4000., 5100.,
+        6600., 9000., 14000.
+    ])
+
+
+@pytest.fixture
+def ferb():
+    return np.array([
+        26.85878, 41.97024, 67.227966, 97.01913, 132.639, 177.97338, 229.7841,
+        289.15055, 361.46968, 456.456, 575.1889, 737.0974, 996.151, 1535.846
+    ])
+
+
+@pytest.fixture
+def fbark():
+    return np.array([
+        0.2, 1.582, 3.701, 5.769, 7.703, 9.579, 11.219, 12.688, 14.106, 15.575,
+        17.02, 18.559, 20.414, 23.061
+    ])
+
+
+@pytest.fixture
+def fmel():
+    return np.array([
+        31.748, 231.994, 503.221, 756.76, 999.986, 1248.812, 1478.826,
+        1695.086, 1912.425, 2146.065, 2383.066, 2642.293, 2962.643, 3431.159
+    ])
+
+
+@pytest.fixture
+def fix_fft():
+    return np.array([
+        0.64125, 5.13, 12.632625, 21.481875, 32.0625, 45.52875, 60.91875,
+        78.553125, 100.035, 128.25, 163.51875, 211.6125, 288.5625, 448.875
+    ])
+
+
+def test_hz2erb(fhz, ferb):
+    np.testing.assert_almost_equal(hz2erb(fhz), ferb, 0)
+
+
+def test_erb2hz(ferb, fhz):
+    np.testing.assert_almost_equal(erb2hz(ferb), fhz, 0)
+
+
+def test_hz2bark(fhz, fbark):
+    np.testing.assert_almost_equal(hz2bark(fhz), fbark, 0)
+
+
+def test_bark2hz(fbark, fhz):
+    np.testing.assert_almost_equal(bark2hz(fbark), fhz, 0)
+
+
+def test_hz2mel(fhz, fmel):
+    np.testing.assert_almost_equal(hz2mel(fhz), fmel, 0)
+
+
+def test_mel2hz(fmel, fhz):
+    np.testing.assert_almost_equal(mel2hz(fmel), fhz, 0)
+
+
+def test_hz2fft(fhz, fix_fft):
+    np.testing.assert_almost_equal(hz2fft(fhz), fix_fft, 0)
+
+
+def test_fft2hz(fix_fft, fhz):
+    np.testing.assert_almost_equal(fft2hz(fix_fft), fhz, 0)
+
+
+def test_fft2erb(fix_fft, ferb):
+    np.testing.assert_almost_equal(fft2erb(fix_fft), ferb, 0)
+
+
+def test_erb2fft(fix_fft, ferb):
+    np.testing.assert_almost_equal(erb2fft(ferb), fix_fft, 0)
+
+
+def test_fft2bark(fix_fft, fbark):
+    np.testing.assert_almost_equal(fft2bark(fix_fft), fbark, 0)
+
+
+def test_bark2fft(fix_fft, fbark):
+    np.testing.assert_almost_equal(bark2fft(fbark), fix_fft, 0)
---
+++ tests/test_utils_preprocessing.py
@@ -0,0 +1,47 @@
+import pytest
+import numpy as np
+from spafe.utils.preprocessing import (zero_handling, pre_emphasis, framing, windowing)
+
+
+@pytest.mark.parametrize('x', [np.arange(4)])
+def test_zero_handling(x):
+    """
+    test zero handling and check if it handles all zero values correctly
+    """
+    y = zero_handling(x=x)
+    # to be implemented
+    assert True
+
+@pytest.mark.parametrize('sig', [np.arange(5)])
+@pytest.mark.parametrize('pre_emph_coeff', [0.97])
+def test_pre_emphasis(sig, pre_emph_coeff):
+    """
+    test if pre_emphasis is applied correctly.
+    """
+    precomputed_result = np.array([0.  , 1.  , 1.03, 1.06, 1.09])
+    np.testing.assert_almost_equal(pre_emphasis(sig=sig, pre_emph_coeff=pre_emph_coeff),
+                                    precomputed_result, 3)
+
+
+@pytest.mark.parametrize('sig', [np.arange(16*3)])
+@pytest.mark.parametrize('fs', [16000])
+@pytest.mark.parametrize('win_len', [0.001])
+@pytest.mark.parametrize('win_hop', [0.001, 0.0005])
+def test_framing(sig, fs, win_len, win_hop):
+    """
+    test if overlapped frames are correctly generated.
+    """
+    frames, frames_length = framing(sig=sig, fs=fs, win_len=win_len, win_hop=win_hop)
+    assert frames.shape == (2 * int(win_len / win_hop), 16)
+    assert frames_length ==  win_len * fs
+
+
+@pytest.mark.parametrize('frames', [[[7, 8 ,9], [1, 2, 3]]])
+@pytest.mark.parametrize('frame_len', [3])
+def test_windowing(frames, frame_len, win_type="hamming"):
+    """
+    test if windowing is applied correctly.
+    """
+    windows = windowing(frames, frame_len, win_type="hamming")
+    for window in windows:
+        assert window[0] < 0.9 and window[-1] < 0.99
