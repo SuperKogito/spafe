dcb38eb7719494c0798485ff3bceb58312b7a5b8
SuperKogito <kogito@hotmail.fr>
2019-11-05 19:03:57 +0100

    updates tests and adjust travis config

--- tests/test_utils_cepstral.py
+++ tests/test_utils_cepstral.py
@@ -2,8 +2,6 @@
 # import numpy as np
 #
 #
-# NFFT = 512
-#
 # def test_cmn(x):
 #
 #
@@ -13,22 +11,3 @@
 # def test_cvn(x):
 #
 # def test_cmvn(x):
-#
-#
-# def test__helper_idx(i, start, stop, step, dct_type):
-#
-# def test__helper_mat(K, ncep, start, stop, step, dct_type):
-# 
-#
-#
-# def test_cep2spec(cep, nfreq, dct_type=2):
-#
-#
-#
-# def test_deltas(x, w=9):
-#
-#
-# def test_spec2cep(spec, ncep=9, dct_type=2):
-#
-#
-# def test_lifter_ceps(cepstra, L=22):
