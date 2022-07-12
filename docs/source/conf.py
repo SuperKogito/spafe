e11c8c5604814651e30d73a2271d38982297d608
SuperKogito <superkogito@gmail.com>
2022-07-12 22:56:51 +0200

    fix version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,9 +25,9 @@ author = "Ayoub Malek"
 html_favicon = "_static/favicon_io/favicon.ico"
 
 # The short X.Y version
-version = "2.0.0"
+version = "0.2.0"
 # The full version, including alpha/beta/rc tags
-release = "2.0.0"
+release = "0.2.0"
 
 # -- General configuration ---------------------------------------------------
 
